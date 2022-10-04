9eaf4145976d066c9670b94785d77428149e9b76
Qyang666 <qingyangli312@gmail.com>
2022-10-04 09:32:28 -0500

    Start!

---
+++ alien_invasion.py
@@ -0,0 +1,6 @@
+import sys
+from alien import Alien 
+import game_functions as gf
+
+##create an instance
+alien = Alien(ai_settings, screen)
---
+++ game_functions.py
@@ -0,0 +1,14 @@
+
+
+
+def uodate_screen(ai_settings,screen,ship,alien,bullets):
+    alien.blitme()
+
+
+
+
+
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):  
+    elif event.key == pygame.K_q:
+        sys.exit()
