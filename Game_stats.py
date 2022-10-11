a07be5830cde5e62815cad5a1b9c6bd15a602169
Qyang666 <qingyangli312@gmail.com>
2022-10-10 21:09:55 -0500

    save

--- Game_stats.py
+++ Game_stats.py
@@ -4,9 +4,10 @@ class GameStats():
         """Initialize statistics."""
         self.ai_settings = ai_settings
         self.reset_stats()
+
          # Start game in an inactive state.
         self.game_active = False
-        self.game_active = True
+    
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
