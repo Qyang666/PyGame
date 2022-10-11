4166e27bc3492812dd281ab01a1a4a88ddddb8f7
Qyang666 <qingyangli312@gmail.com>
2022-10-10 20:45:29 -0500

    Save

--- Game_stats.py
+++ Game_stats.py
@@ -4,6 +4,8 @@ class GameStats():
         """Initialize statistics."""
         self.ai_settings = ai_settings
         self.reset_stats()
+         # Start game in an inactive state.
+        self.game_active = False
         self.game_active = True
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,23 @@
+#import modules 
 import pygame
 from settings import Settings
 from pygame.sprite import Group
 from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
+from button import Button
 
+#start a game
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize game and 
     pygame.init()
+    # create screen 
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    # add screen caption
     pygame.display.set_caption("Alien Invasion")
+    #add a button
+    play_button = Button(ai_settings, screen, "Play")
 
     ## Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -31,8 +38,10 @@ def run_game():
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            
         #Make the aliens move 
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         ## update images on the screen and flip to the new screen
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
