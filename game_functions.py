a5b414d0e486546c7df39cd545e256a6e9310c31
Qyang666 <qingyangli312@gmail.com>
2022-10-11 00:10:13 -0500

    sss

--- game_functions.py
+++ game_functions.py
@@ -96,10 +96,12 @@ def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
     check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, stats,ship, sb, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
     if collisions:
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
@@ -110,16 +112,18 @@ def check_bullet_alien_collisions(ai_settings, screen, stats,ship, sb, aliens, b
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
+
         # Increase level.
         stats.level += 1
         sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""
     # Create a new bullet and add it to the bullets group.
-    if len(bullets) < ai_settings.bullets_allowed:
+    if len(bullets) < ai_settings.ship_limit:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
